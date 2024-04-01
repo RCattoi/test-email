05e53fc73dae019587957e2bda6708dd36b3f937
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-31 22:34:55 -0300

    fix: api endpoint

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const btn = document.querySelector('.enviarEmail');
 const callback = async (e, text) => {
   e.preventDefault();
 
-  const fetchEmail = await fetch('https://localhost:3000/api/data', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      to: 'rodrigoccattoi@gmail.com',
-      subject: `Novo Contato`,
-      name: 'Rodrigo',
-      message: 'mensagem de teste',
-      reply_to: 'felipehgn@gmail.com',
-    }),
-  });
+  const fetchEmail = await fetch(
+    'https://email-transportadora.onrender.com/api/data',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: 'rodrigoccattoi@gmail.com',
+        subject: `Novo Contato`,
+        name: 'Rodrigo',
+        message: 'mensagem de teste',
+        reply_to: 'felipehgn@gmail.com',
+      }),
+    }
+  );
 
   const json = await fetchEmail.json();
   console.log(json);
