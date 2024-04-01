3c1b41519679130b51e3435a8e2015cdfc30c47b
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-31 22:20:00 -0300

    feat: update email sending logic

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-import USERID from './email-key.js';
 const btn = document.querySelector('.enviarEmail');
 
-emailjs.init(USERID); // Your user ID esconder na aplicação
-const callback = (e, text) => {
+const callback = async (e, text) => {
   e.preventDefault();
-  // const name = document.querySelector('.name').value;
-  emailjs
-    .send('service_dzoqy8o', 'template_2akyqfb', {
+
+  const fetchEmail = await fetch('https://localhost:3000/api/data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
       to: 'rodrigoccattoi@gmail.com',
       subject: `Novo Contato`,
       name: 'Rodrigo',
-      text: 'mensagem de teste',
+      message: 'mensagem de teste',
       reply_to: 'felipehgn@gmail.com',
-    })
-    .then(function (response) {
-      console.log('Email sent successfully:', response);
-    })
-    .catch(function (error) {
-      console.error('Email sending failed:', error);
-    });
+    }),
+  });
+
+  const json = await fetchEmail.json();
+  console.log(json);
+
+  // const name = document.querySelector('.name').value;
+  // emailjs
+  //   .send('service_dzoqy8o', 'template_2akyqfb', {
+  //     to: 'rodrigoccattoi@gmail.com',
+  //     subject: `Novo Contato`,
+  //     name: 'Rodrigo',
+  //     message: 'mensagem de teste',
+  //     reply_to: 'felipehgn@gmail.com',
+  //   })
+  //   .then(function (response) {
+  //     console.log('Email sent successfully:', response);
+  //   })
+  //   .catch(function (error) {
+  //     console.error('Email sending failed:', error);
+  //   });
 };
+
 btn.addEventListener('click', callback);
